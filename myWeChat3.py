81c916e57f98b712bc7f478e084c363ba74265c4
weilun1985 <409297393@qq.com>
2023-12-02 19:48:23 +0800

    20231202-2

--- myWeChat3.py
+++ myWeChat3.py
@@ -1,3 +1,5 @@
+import traceback
+
 import uiautomation as auto
 import logging,math,os,queue,re,time
 from uiautomation import WindowControl,ListControl,ButtonControl,TextControl,EditControl,ListItemControl,DocumentControl,PropertyId
@@ -21,6 +23,8 @@ class MessageInfo:
         self.MsgContent=None
         self.Sender:str=None
         self.MsgContentType:str=None
+        self.MsgFullGet:bool=False
+        self.MsgFetchError:str=None
 
 class WeChatUtil:
     @staticmethod
@@ -93,27 +97,55 @@ class WeChatUtil:
     @staticmethod
     def parse_message_info(listItem:ListItemControl):
         name=listItem.Name
-        msg_content = name
-
+        # 获取发送人按钮
         sender_btn = listItem.ButtonControl(searchDepth=2)
         if not sender_btn.Exists(maxSearchSeconds=0):
             # 没有发送者，可能是系统消息
             # 判断是否是时间
-            time_split=WeChatUtil.str_to_time(msg_content)
+            time_split=WeChatUtil.str_to_time(name)
             return time_split
+        sender_name=sender_btn.Name
+        # 获取内容区域
+        if sender_btn.GetPreviousSiblingControl() is None:
+            rect_main=sender_btn.GetNextSiblingControl().GetLastChildControl().GetFirstChildControl().GetFirstChildControl() # 3级
+        else:
+            rect_main=sender_btn.GetPreviousSiblingControl().GetFirstChildControl().GetFirstChildControl().GetFirstChildControl() #3级
+        # 获取内容区域按钮，内容区域的按钮特征是按钮面积最大
+        btn_max_area=0
+        btn_main=None
+        for n in range(1,5):
+            btn=rect_main.ButtonControl(foundIndex=n)
+            if not btn.Exists(maxSearchSeconds=0):
+                break
+            if btn.BoundingRectangle is not None:
+                area=btn.BoundingRectangle.width()*btn.BoundingRectangle.height()
+                if area==0:
+                    continue
+                # print(n,'. ',btn.BoundingRectangle)
+                if area>btn_max_area:
+                    btn_max_area=area
+                    btn_main=btn
+
+        msg_content = []
+        for n in range(1,5):
+            txc = rect_main.TextControl(foundIndex=n)
+            if txc.Exists(maxSearchSeconds=0):
+                msg_content.append(txc.Name)
 
         msg_info=MessageInfo()
         msg_info.Sender=sender_btn.Name
         msg_info.MsgContent=msg_content
-        msg_info.MsgContentType='文本'
-        log.info(f'start-fetch-msg: {msg_info.Sender} {msg_info.MsgContentType}')
+
         if name=='[链接]':
             msg_info.MsgContentType='链接'
-            open_btn=listItem.ButtonControl(foundIndex=2)
-            if open_btn.Exists(maxSearchSeconds=0):
-                open_btn.Click()
-            page_info=WeChatUtil.fetch_page_info()
-            msg_info.MsgContent=page_info
+            btn_main.Click(simulateMove=False)
+            remark=f'sender={msg_info.Sender} topic={name}'
+            page_info,emsg=WeChatUtil.fetch_page_info(remark)
+            if page_info is not None:
+                msg_content.append(page_info)
+                msg_info.MsgFullGet=True
+            elif emsg is not None:
+                msg_info.MsgFetchError=emsg
         elif name=='[视频]':
             msg_info.MsgContentType = '视频'
             pass
@@ -129,31 +161,43 @@ class WeChatUtil:
         elif name == '[音乐]':
             msg_info.MsgContentType = '音乐'
             pass
+        else:
+            msg_info.MsgContentType = '文本'
+            msg_info.MsgFullGet=True
+        name1=name.replace('\n',' ')
+        log.info(
+            f'fetch-msg: sender={msg_info.Sender} topic={name1} full-get={msg_info.MsgFullGet} btn-main-found:{btn_main != None} {btn_main.BoundingRectangle if btn_main != None else None} content:{msg_content}')
         return msg_info
 
     @staticmethod
-    def fetch_page_info():
+    def fetch_page_info(remark):
         wx_browser=auto.PaneControl(searchDepth=1, Name='微信', ClassName='Chrome_WidgetWin_0')
         if not auto.WaitForExist(wx_browser, 10):
-            log.error('wx_browser not exists!')
-            return
+            emsg =f'wx_browser not exists! {remark}'
+            log.error(emsg)
+            return None,emsg
         # wx_browser.SetActive()
         doc=wx_browser.Control(ClassName='Chrome_RenderWidgetHostHWND')
         if not doc.Exists(maxSearchSeconds=10):
-            log.error('wx_browser document not exists!')
-            return
+            emsg=f'wx_browser document not exists! {remark}'
+            log.error(emsg)
+            return None,emsg
         gc=doc.GroupControl()
         if gc.Exists(maxSearchSeconds=30):
             title=doc.Name
             url=doc.GetPropertyValue(PropertyId.LegacyIAccessibleValueProperty)
-            log.info('fetch-page:',title,url)
+            log.info(f'fetch-page: {title} {url}')
             tabs=wx_browser.TabControl().TabItemControl().GetParentControl().GetChildren()
             if len(tabs)>1:
                 tab = tabs[-1]
                 btn_close = tab.ButtonControl(Name='关闭')
                 if btn_close.Exists(maxSearchSeconds=2):
                     btn_close.Click(simulateMove=False)
-            return {'title':title,'url':url}
+            return {'title':title,'url':url},None
+        else:
+            emsg = f'wx_browser document load timeout! {remark}'
+            log.error(emsg)
+            return None,emsg
 
 # 以下是主要微信操作部分
 def __get_wx_win():
@@ -161,22 +205,35 @@ def __get_wx_win():
     if wechat.Exists():
         return wechat
 
-def __search(wechat:WindowControl,key):
+def __search_session(wechat:WindowControl, key):
     search = wechat.EditControl(Name='搜索')
     search.Click(simulateMove=False)
     auto.SetClipboardText(key)
-    search.SendKeys('{Ctrl}v')
-    search.SendKeys('{Enter}')
+    search.SendKeys('{Ctrl}a')
+    search.SendKeys('{Ctrl}v',waitTime=0.1)
 
-def search(key):
+    resltListCtl=wechat.ListControl(RegexName='@str:IDS_FAV_SEARCH_RESULT:')
+    if not auto.WaitForExist(resltListCtl,2):
+        return False
+    r1=resltListCtl.TextControl(Name='联系人',Depth=2)
+    r2=resltListCtl.TextControl(Name='群聊',Depth=2)
+    if r1 is None and r2 is None:
+        return False
+
+
+    search.SendKeys('{Enter}',waitTime=0.1)
+    log.info(f"wechat search: {key}")
+    return True
+
+def search_session(key):
     wechat = __get_wx_win()
     wechat.SetActive()
-    __search(wechat, key)
+    __search_session(wechat, key)
 
 def send_msg(session_name,msg):
     wechat=__get_wx_win()
     wechat.SetActive()
-    __search(wechat,session_name)
+    __search_session(wechat, session_name)
     edit = wechat.EditControl(Name=session_name)
     if edit.Exists():
         auto.SetClipboardText(msg)
@@ -221,15 +278,142 @@ def get_session_list():
             scrollCtl.SetScrollPercent(horizontalPercent=0, verticalPercent=(i + 1) * show_percent)
     return session_list
 
+def current_chat(wechat)->str:
+    session_name=None
+    s_topic_btn = wechat.ButtonControl(Name='聊天信息')
+    if s_topic_btn.Exists(maxSearchSeconds=0):
+        topic = s_topic_btn.GetParentControl().GetParentControl().GetPreviousSiblingControl().TextControl()
+        session_name = topic.Name
+        if re.match('^(.+)\s\(\d+\)$', topic.Name) != None:
+            session_name = topic.Name.rsplit(' ', 2)[0]
+    return session_name
+
 def get_message_list(session_info:SessionInfo=None,time_util=None,max_count=0)->List[MessageInfo]:
     wechat = __get_wx_win()
     wechat.SetActive()
-    __search(wechat, session_info.SessionName)
+    sess_name = current_chat(wechat)
+    if session_info is not None and sess_name!=session_info.SessionName:
+        __search_session(wechat, key=session_info.SessionName)
+        sess_name = current_chat(wechat)
+        if sess_name != session_info.SessionName:
+            err = f'会话可能窜了: {sess_name}  {session_info.SessionName}'
+            log.warning(err)
+            raise Exception(err)
+
     #获取会话列表
+    msgListCtl=wechat.ListControl(Name='消息')
+    msgListCtl.Click()
+    scrollCtl = msgListCtl.GetScrollPattern()
+    # 存在滚动条且可以垂直滚动
+    if scrollCtl is not None and scrollCtl.VerticallyScrollable:
+        # 先一滚到顶，看看隐藏区域占比多少
+        scrollCtl.SetScrollPercent(horizontalPercent=0, verticalPercent=0)
+        hidd_percent = scrollCtl.VerticalScrollPercent  # 隐藏区域占比
+        show_percent = 1 - hidd_percent  # 可见区域占比
+        # 滚回底部
+        scrollCtl.SetScrollPercent(horizontalPercent=0, verticalPercent=1)
+    msg_list: List[MessageInfo] = []
+    list0 = msgListCtl.GetChildren()
+    # print('got messages size:',len(list0))
+    if list0 is None or len(list0) == 0:
+        return msg_list
+
+    current_list=msgListCtl.GetChildren()
+    # for itm in current_list:
+    #     print(f'{itm.Name}-rect-0:', itm.BoundingRectangle)
+
+    current_msg = current_list[-1]
+    while True:
+        # 如果设置了信息获取最大条数，则只取最大条数信息
+        if max_count > 0 and len(msg_list) >= max_count:
+            break
+        # print(f'{current_msg.Name}-rect-0:', current_msg.BoundingRectangle)
+
+        msg_info = WeChatUtil.parse_message_info(current_msg)
+        if msg_info is not None:
+            if isinstance(msg_info, datetime):
+                # 将之前没有时间的消息都标注为这个时间
+                time_c = msg_info
+                for itm in msg_list:
+                    if itm.MsgTime is not None:
+                        break
+                    itm.MsgTime = time_c
+                    itm.MsgStrTime = current_msg.Name
+                # 如果设置了信息获取截止时间，则只取之后的信息
+                if time_util != None and time_c < time_util:
+                    break
+            elif isinstance(msg_info, MessageInfo):
+                msg_info.SessionName = sess_name
+                if len(msg_list) == 0:
+                    msg_list.append(msg_info)
+                    if session_info != None:
+                        msg_info.MsgTime = session_info.LastMsgTime
+                else:
+                    msg_list.insert(0, msg_info)
+        # 上翻聊天记录
+        befor = current_msg.GetPreviousSiblingControl()
+        if befor is not None and befor.Exists():
+            if isinstance(befor, ButtonControl):
+                print('--------->', befor.Name)
+                befor.Click(simulateMove=False, waitTime=0.1)
+                print('reget-prev-is: ', current_msg.GetPreviousSiblingControl())
+                break
+            else:
+                height = current_msg.BoundingRectangle.height()
+                current_msg = befor
+                # 上滑区域
+                msgListCtl.WheelUp(height)
+        else:
+            break
+    # 再清理一轮消息
+    if time_util != None:
+        temp_list = []
+        for msg in msg_list:
+            if msg.MsgTime < time_util:
+                continue
+            temp_list.append(msg)
+        msg_list = temp_list
+    if max_count > 0 and len(msg_list) > max_count:
+        n = len(msg_list) - max_count
+        temp_list = msg_list[n:]
+        msg_list = temp_list
+    return msg_list
+
+
+def monitor_new_msg(handler):
+    seslist0 = get_session_list()
+    seslist1 = []
+    # 获取有新信息的会话
+    for ses in seslist0:
+        if ses.NewMsgNum > 0:
+            seslist1.append(ses)
+    # 获取新信息
+    for ses in seslist1:
+        try:
+            log.info(f'session={ses.SessionName} new_msg_cnt={ses.NewMsgNum} last_msg_time={ses.LastMsgTime}')
+
+            new_msg_list = get_message_list(session_info=ses, max_count=ses.NewMsgNum)
+            ses.MessageList = new_msg_list
+            # 触发回调
+            for msg in new_msg_list:
+                try:
+                    handler(ses, msg)
+                except Exception as e:
+                    log.error(e,exc_info=True,stack_info=True)
+        except Exception as e:
+            log.error(e,exc_info=True,stack_info=True)
+    pass
+
 
 
 def test_2():
-    print()
+    msglist = get_message_list(session_info=None)
+    a = 1
+    for msg in msglist:
+        if msg.MsgFullGet==False:
+            print(msg.__dict__)
+        a += 1
+    pass
 
 def test_3():
     list = get_session_list()
@@ -241,9 +425,28 @@ def test_3():
 def test_4():
     send_msg('我我我',f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
 
+
+def test_5():
+    mq = queue.Queue()
+    def fun(ses, msg):
+        print(f'received: {msg.__dict__}')
+        mq.put(msg)
+
+    while True:
+        print('start check new msg...')
+        monitor_new_msg(fun)
+        print(f'start reply msg, mq_size={mq.qsize()}')
+        for i in range(mq.qsize()):
+            msg = mq.get_nowait()
+            json.encoder()
+            re_msg = f'reply for "{msg.MsgContentType} - {len(msg.MsgContent)}" by "{msg.Sender}" at {msg.MsgTime.strftime("%Y-%m-%d %H:%m")}'
+            send_msg(msg.SessionName, re_msg)
+        time.sleep(5)
+    pass
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s [%(name)s] [%(process)d] [%(thread)d] %(levelname)s - (%(funcName)s) -> %(message)s')
     print('start at:',datetime.today().date().strftime('%Y%m%d %H:%M:%S'))
-    test_3()
+    test_5()
     pass
