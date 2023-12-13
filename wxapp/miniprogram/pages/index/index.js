6c22d2b901b8c05681bcce8fc4502bcc87bf0282
weilun1985 <409297393@qq.com>
2023-12-13 23:28:40 +0800

    20231213-2

--- wxapp/miniprogram/pages/index/index.js
+++ wxapp/miniprogram/pages/index/index.js
@@ -5,25 +5,28 @@ const { downloadFile, downloadFiles } = require('../../utils')
 
 Page({
     data: {
-        //   videoUrl:'https://vd4.bdstatic.com/mda-pjsz90g40y6rt7eh/1080p/cae_h264/1698452902421847957/mda-pjsz90g40y6rt7eh.mp4?pd=-1&pt=1&cr=3&vt=0&cd=0&did=cfcd208495d565ef66e7dff9f98764da&logid=1168090665&vid=11310265590257684878&auth_key=1700639368-0-0-6ef6ba22abdc0ba6b3a3df0aaf6fa9e1&bcevod_channel=searchbox_feed',
-        // title: '猫女机器人',
-        // descp: '马斯克研发出了猫女机器人，准备量产，服务广大单身男性',
-        // video: {
-        //     url: 'https://sns-video-bd.xhscdn.com/stream/110/258/01e5594329b536aa010370038be4b08976_258.mp4',
-        //     image:'',
-        // },
-        // image:{
-        //     urls:[
-        //         'http://sns-webpic-qc.xhscdn.com/202312100041/740a2137025190c1aeff5fb2e4893f97/1040g00830saeo364iq405p0taeg4i536rfbbn88!nd_whgt34_webp_wm_1',
-        //         'http://sns-webpic-qc.xhscdn.com/202312100041/e61146a9bba4e93b6e1e6d5ccff65088/1040g00830saeo364iq3g5p0taeg4i5360qgl2f0!nd_whgt34_webp_wm_1',
-        //     ],
-        // },
-        swiperCurrent:0,
-        savePercent:0
+        id:null
     },
-    onLoad:function(){
-        console.log('on load.');
-        this.loadResInfo();
+    onLoad:function(options){
+        console.log('on load: options='+JSON.stringify(options));
+        if(options&&options.id){
+            let id=options.id;
+            this.setData({id:id});
+            this.loadResInfo(id);
+        }
+    },
+    submitResId(){
+        var id=this.data.id;
+        if(id&&id.length>0){
+            console.log('To get res: '+id);
+            url= `/pages/index/index?id=${id}`
+            console.log(url)
+            wx.redirectTo({
+                url: url,
+            })
+        }else{
+            console.warn('no id');
+        }
     },
     //轮播图的切换事件
     swiperChange: function (e) {
@@ -44,13 +47,13 @@ Page({
             url: this.data.image.urls[this.data.swiperCurrent]
         })
     },
-    loadResInfo(){
+    loadResInfo(resid){
         wx.showLoading({
           title: '资源信息正在加载中...',
           mask:true
         })
         wx.request({
-          url: 'https://1e63211h01.yicp.fun/res?id=486846d69339a77cf6460b6ce74fe4b4',
+          url: 'https://1e63211h01.yicp.fun/res?id='+resid,
           method:'GET',
           header:{'content-type':'application/json'},
           success:(res)=>{
@@ -65,6 +68,8 @@ Page({
                     info+=data.descp;
                 }
                 data.info=info;
+                data.swiperCurrent=0;
+                data.savePercent=0;
                 if(data.res_type==='video'){
                     let url=data.video.url;
                     if(url.startsWith('http://')){
